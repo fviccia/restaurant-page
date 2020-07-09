148fa51528d2de02eb505c6b2c1152ee358a109d
Franco Viccia <francoviccia@gmail.com>
2020-07-09 16:03:59 -0300

    Added Buttons Bar

--- src/homePage.js
+++ src/homePage.js
@@ -4,21 +4,18 @@ const homePage = () => {
   center.id = "center";
   let headline = document.createElement("div");
   headline.id = "headline";
-  let buttons = document.createElement("div");
-  buttons.id = "buttons";
   let description = document.createElement("div");
   description.id = "description";
   let headlineContent = "Tomato Surprise";
   let descriptionContent = `If you are conscious about what you eat and the impact that this
   haves to our world you have come to the right place. Pick whatever you want
   from our daily harvested fruit and vegetables, then you can go to the
-  cashier to pay a all inclusive rate of USD 10 and to receive our
-  special surprise of the day.`;
+  cashier to pay an all inclusive rate of USD 10. Also you will receive our
+  special surprise of the day, because we are Tomato Surprise!!!`;
   headline.textContent = headlineContent;
   description.textContent = descriptionContent;
   content.appendChild(center);
   center.appendChild(headline);
-  center.appendChild(buttons);
   center.appendChild(description);
 };
 
